0e415838022ecb34ed8c98e881fa8a2ebd9dab11
ji-0o0o0o <tlswldud28@naver.com>
2022-10-16 22:10:21 +0900

    feat level0 두수의 차 [1]

---
+++ src/main/java/programmers/level0/GetTheRest.java
@@ -0,0 +1,20 @@
+package programmers.level0;
+
+public class GetTheRest {
+    public static void main(String[] args) {
+
+        Solution sol = new Solution();
+        System.out.println(sol.solution(2,3));
+        System.out.println(sol.solution(100,2));
+
+
+
+    }
+
+    static class Solution {
+        public int solution(int num1, int num2) {
+            int answer = num1-num2;
+            return answer;
+        }
+    }
+}
