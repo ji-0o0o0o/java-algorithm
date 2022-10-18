154055bc6196833466b539cb7c33bc69744664d6
ji-0o0o0o <tlswldud28@naver.com>
2022-10-18 18:47:04 +0900

    feat : 각도기 [1]

--- src/main/java/programmers/level0/Protractor.java
+++ src/main/java/programmers/level0/Protractor.java
@@ -17,7 +17,7 @@ public class Protractor {
                     answer = 1;
                 } else if (angle == 90) {
                     answer = 2;
-                } else if (90<angle&&angle<180) {
+                } else if (angle<180) {
                     answer = 3;
                 } else {
                     answer = 4;
