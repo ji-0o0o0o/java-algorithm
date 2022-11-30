05f0448d7e988f395428572d2f67d7ac2f36113d
ji-0o0o0o <tlswldud28@naver.com>
2022-11-30 17:54:32 +0900

    221130

---
+++ src/main/java/paractice/ch01/TripleL.java
@@ -0,0 +1,61 @@
+package paractice.ch01;
+
+import java.util.Scanner;
+
+
+public class TripleL {
+    //메서드 이용하여 왼쪽아래 직각인 이등변삼각형 출력
+    static void triangleLB(int n){
+
+        for (int i = 1; i <=n ; i++) {
+            for (int j = 1; j <=i ; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    //왼쪽위가 직각인 이동변삼각형
+    static void triangleLU(int n){
+        for (int i = 1; i <=n ; i++) {
+            for (int j = 1; j <=n-i+1 ; j++) {//j가 1일때 n-i+1개의 별이 찍힘
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    //오른쪽 위가 직각인 이등변삼각형
+    static void triangleRU(int n){
+        for (int i = 1; i <=n ; i++) {
+            System.out.print(" ".repeat(i-1));
+            for (int j = 1; j <=n-i+1 ; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        }
+    //오른쪽 아래가 직각인 이등변삼각형
+    static void triangleRB(int n){
+        for (int i = 1; i <=n ; i++) {
+            System.out.print(" ".repeat(n-i));
+            for (int j = 1; j <=i ; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("단수는? ");int a =sc.nextInt();
+        System.out.println("왼쪽 아래가 직각인 이동변삼각형 ");
+        triangleLB(a);
+        System.out.println("왼쪽 위가 직각인 이동변삼각형");
+        triangleLU(a);
+        System.out.println("오른쪽 아래가 직각인 이동변삼각형");
+        triangleRB(a);
+        System.out.println("오른쪽 위가 직각인 이동변삼각형");
+        triangleRU(a);
+    }
+}
+
+
