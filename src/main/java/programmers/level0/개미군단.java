a8549539aaf83868d04b7db31aa328e93b715ff2
ji-0o0o0o <tlswldud28@naver.com>
2022-12-15 23:44:43 +0900

    feat : 개미군단[1]

---
+++ src/main/java/programmers/level0/개미군단.java
@@ -0,0 +1,31 @@
+package programmers.level0;
+
+public class 개미군단 {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.solution(23));
+    }
+    static class Solution {
+        public int solution(int hp) {
+            int cnt = 0;
+                if (hp>=5){
+                    cnt+=hp/5;
+                    hp%=5;
+                }
+            if (hp>=3){
+                    cnt+=hp/3;
+                hp%=3;
+                }
+            if (hp>=1){
+                    cnt+=hp/1;
+                }
+            return cnt;
+        }
+    }
+    //다른예시
+    static class Solution1 {
+        public int solution(int hp) {
+            return hp / 5 + (hp % 5 / 3) + hp % 5 % 3;
+        }
+    }
+}
