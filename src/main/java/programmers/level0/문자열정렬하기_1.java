08e808d9d83836a6797df21a1940e20fd5d84703
ji-0o0o0o <tlswldud28@naver.com>
2022-12-18 15:05:07 +0900

    feat : 문자열 정렬하기_1[1]

--- src/main/java/programmers/level0/문자열정렬하기_1.java
+++ src/main/java/programmers/level0/문자열정렬하기_1.java
@@ -1,20 +1,44 @@
 package programmers.level0;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class 문자열정렬하기_1 {
     public static void main(String[] args) {
         Solution solution= new Solution();
         System.out.println(solution.solution("hi12392"));
+        System.out.println(solution.solution("p2o4i8gj2"));
+        System.out.println(solution.solution("abcde0"));
     }
     static class Solution {
-        public String solution(String my_string) {
-            StringBuilder answer = new StringBuilder();
-            for (int i = 0; i <my_string.length() ; i++) {
-                if (my_string.charAt(i)<=9){
-                    System.out.println(my_string.charAt(i));
-                    answer.append((my_string.charAt(i)));
-                }
+        public int[] solution(String my_string) {
+            String[] nums = my_string.replaceAll("[^0-9]", "").split("");
+            int[] answer = new int[nums.length];
+            for (int i = 0; i < nums.length; i++) {
+                answer[i] = Integer.parseInt(nums[i]);
             }
-            return answer.toString();
+            Arrays.sort(answer);
+            return answer;
+        }
+        public int[] solution2(String myString) {
+            return Arrays.stream(myString.replaceAll("[A-Z|a-z]", "").split("")).sorted().mapToInt(Integer::parseInt).toArray();
+        }
+        public int[] solution3(String my_string) {
+
+            my_string = my_string.replaceAll("[a-z]","");
+
+            int[] answer = new int[my_string.length()];
+
+            for(int i =0; i<my_string.length(); i++){
+                answer[i] = my_string.charAt(i) - '0';
+            }
+
+            Arrays.sort(answer);
+
+            return answer;
         }
     }
-}
+    }
+
