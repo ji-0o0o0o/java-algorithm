3b594e54534760e1a4d90213a69b6e262854157a
ji-0o0o0o <tlswldud28@naver.com>
2022-12-19 21:16:16 +0900

    feat : 팩토리얼[5]

---
+++ src/main/java/programmers/level0/팩토리얼.java
@@ -0,0 +1,43 @@
+package programmers.level0;
+
+public class 팩토리얼 {
+    /**
+     * i팩토리얼 (i!)은 1부터 i까지 정수의 곱을 의미합니다. 예를들어 5! = 5 * 4 * 3 * 2 * 1 = 120 입니다. 정수 n이 주어질 때 다음 조건을 만족하는 가장 큰 정수 i를 return 하도록 solution 함수를 완성해주세요.
+     *
+     *     i! ≤ n
+     *     제한사항
+     *     0 < n ≤ 3,628,800
+     *     입출력 예
+     *     n	result
+     *     3628800	10
+     *     7	3
+     *     입출력 예 설명
+     *     입출력 예 #1
+     *
+     *     10! = 3,628,800입니다. n이 3628800이므로 최대 팩토리얼인 10을 return 합니다.
+     *     입출력 예 #2
+     *
+     *     3! = 6, 4! = 24입니다. n이 7이므로, 7 이하의 최대 팩토리얼인 3을 return 합니다.
+    **/
+    static class Solution {
+        public int solution(int n) {
+            int answer = 1;
+            int i;
+                for (i = 1;true ; i++) {
+                    answer*=i;
+                    if (answer>n){
+                        i-=1;
+                        break ;
+                    }
+                    if (answer == n)break;
+                }
+            return i;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.solution(3628800));
+        System.out.println(solution.solution(7));
+    }
+}
